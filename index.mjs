e0c7fd0f413eab786a76c67cacfd7889e3d5249c
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-04-20 13:22:26 +0200

    everblack, tru4d

---
+++ index.mjs
@@ -0,0 +1,10 @@
+/**
+ *
+ *
+ * @author: Bernhard Lukassen
+ */
+
+export { default as Vault }             from './lib/vault/vault.mjs';
+export { default as VaultStore }        from './lib/vault/storage/vaultstore.mjs';
+export { default as VaultMemStore }     from './lib/vault/storage/vaultmemstore.mjs';
+export { default as VaultFileStore }    from './lib/vault/storage/vaultfilestore.mjs';
