e92f5b58cecff0bca9d2673f4cabee2815029e3b
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-04-21 21:40:22 +0200

    everblack, tru4d

--- index.reliant.mjs
+++ index.reliant.mjs
@@ -4,11 +4,13 @@
  * @author: Bernhard Lukassen
  */
 
-import Everblack                        from "./lib/everblack.mjs";
+// todo: check what functions from everblack will be needed on reliant nodes
+import Everblack                        from "./lib/reliant/everblack.mjs";
 
+/*
 export { default as Vault }             from './lib/vault/vault.mjs';
 export { default as VaultStore }        from './lib/vault/storage/vaultstore.mjs';
 export { default as VaultMemStore }     from './lib/vault/storage/vaultmemstore.mjs';
-export { default as VaultFileStore }    from './lib/vault/storage/vaultfilestore.mjs';
+*/
 
 export const service = new Everblack();
