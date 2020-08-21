56e915fa17d109af2f0fc8e9000b058082ddc14c
Bernhard Lukassen <bl@bernhard-lukassen.com>
2020-08-21 11:24:48 +0200

    everblack refactorings

--- index.reliant.mjs
+++ index.reliant.mjs
@@ -13,4 +13,7 @@ export { default as VaultStore }        from './lib/vault/storage/vaultstore.mjs
 export { default as VaultMemStore }     from './lib/vault/storage/vaultmemstore.mjs';
 */
 
+export { default as SharedCrypto }      from './lib/identity/sharedcrypto.mjs';
+export { default as IdentityShim }      from './lib/identity/identityshim.mjs';
+
 export const service = new Everblack();
