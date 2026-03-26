0812a66e8be159a8f60e28997a8d592bf6ce0be7
Bernhard Lukassen <git@bernhard-lukassen.com>
2026-03-26 13:07:18 +0100

    preparations for UI package, fixes

--- index.mjs
+++ index.mjs
@@ -8,12 +8,12 @@ import Everblack                        from "./lib/everblack.mjs";
 
 // export { default as GunAdapter }        from './lib/gunadapter.mjs';
 
-export { default as Vault }             from './lib/vault/vault.mjs';
-export { default as VaultStore }        from './lib/vault/storage/vaultstore.mjs';
-export { default as VaultMemStore }     from './lib/vault/storage/vaultmemstore.mjs';
-export { default as VaultFileStore }    from './lib/vault/storage/vaultfilestore.mjs';
+// export { default as Vault }             from './lib/vault/vault.mjs';
+// export { default as VaultStore }        from './lib/vault/storage/vaultstore.mjs';
+// export { default as VaultMemStore }     from './lib/vault/storage/vaultmemstore.mjs';
+// export { default as VaultFileStore }    from './lib/vault/storage/vaultfilestore.mjs';
 
-export { default as SharedCrypto }      from './lib/identity/sharedcrypto.mjs';
+// export { default as SharedCrypto }      from './lib/identity/sharedcrypto.mjs';
 export { default as IdentityShim }      from './lib/identity/identityshim.mjs';
 
 /*
